9defbc950733a446d4a4c5c71b4ec7bcb84ec494
Tony Hinkle <Tony Hinkle>
2016-01-16 21:33:34 -0500

    completed basic functionality

--- script.js
+++ script.js
@@ -1,35 +1,87 @@
 $(document).ready(function ($) {
 
     sequenceArray = [];
+    playerArray = [];
+    simonsTurn = true;
     
     $("td").on("click", function(){
-		$(this).css("opacity", "1").fadeTo(600, ".25");
+        
+        if(simonsTurn === true){
+            return;
+        }
+
+        lightItUp([this, false]);
+        playerArray.push($(this).data("square"));
+        console.log(playerArray);
+        comparePlayerArray();
+        
 	});
+    
+    function comparePlayerArray(){
+     
+        for(var i = 0; i < playerArray.length; i++){
+            if(playerArray[i] != sequenceArray[i]){
+                //player loses...
+                //console.log(sequenceArray);
+                //console.log(playerArray);
+                simonsTurn = true;
+                $("#btnStart").text("Start");
+                alert("You lose after " + (sequenceArray.length - 1));
+                sequenceArray = [];
+                break;
+            }
+        }
+        
+        if(playerArray.length == sequenceArray.length){
+            simonsTurn = true;
+            $("#btnStart").prop("disabled", false);
+            playerArray = [];
+        }
+    }
 	
 	$("#btnStart").on("click", function(){
 		
+        simonsTurn = true;
         $(this).text("Go");
         var cellId = "";
+        var arrPassThru = [];
+                
+        $(this).prop("disabled", true);
         
         //Generate a random number between 1 and 4 and push it into sequenceArray
         sequenceArray.push(Math.floor((Math.random() * 4) + 1));
 
         //Loop through sequenceArray and light up corresponding table cells
         for(var i = 0; i < sequenceArray.length; i++){
-            cellId = "#c" + sequenceArray[i];
-            createInterval(lightItUp, cellId, i * 1000);
+            //First element in the array to pass is the table cell's id
+            arrPassThru.push("#c" + sequenceArray[i]);
+            
+            //Second element in the array is boolean whether it is the last element in simon's array
+            arrPassThru.push(i == (sequenceArray.length - 1));
+            
+            createInterval(lightItUp, arrPassThru, i * 1000);
+            arrPassThru = [];
         }
         
 	});
     
     function createInterval(f,dynamicParameter,interval) {
+        //This function enables variables to be passed through to a function through setTimeout
         setTimeout(function() {
             f(dynamicParameter); 
         }, interval); 
     }
     
-    function lightItUp(c){
-        $(c).css("opacity", "1").fadeTo(600, ".25");
+    function lightItUp(arr){
+        //first element in arr is the id of the element to light up.
+        //second element in arr is if this is the last element simon will be lighting up.
+        $(arr[0]).fadeTo(200, 1).fadeTo(600, ".25");
+        
+        //If it was simon's last cell to light up
+        if(arr[1] === true){
+            simonsTurn = false;
+            //$("#btnStart").prop("disabled", false);
+        }
     }
         
 });
